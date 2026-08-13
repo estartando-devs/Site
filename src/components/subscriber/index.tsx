d9a5936c61f5550c60d6d3d59617c92069f2f343
Ramon Xavier <ramon.xavier@g.globo>
2026-08-13 17:42:53 -0300

    liberado

--- src/components/subscriber/index.tsx
+++ src/components/subscriber/index.tsx
@@ -49,7 +49,7 @@ const COURSE_TO_API: Record<string, 'web' | 'backend' | 'uiux'> = {
 };
 
 const year = new Date().getFullYear();
-const IS_REGISTRATION_CLOSED = true;
+const IS_REGISTRATION_CLOSED = false;
 
 const steps = [
   <Compass key="trilha" />, // 0
